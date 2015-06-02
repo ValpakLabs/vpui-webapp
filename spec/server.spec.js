422522b4d03176c24c006598c1df3ceecf7e249a
Darek Rossman <darek_rossman@valpak.com>
2015-06-02 11:47:45 -0400

    add tests

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,19 +1,38 @@
 var assert = require('assert');
 var http = require('http');
+var superagent = require('superagent');
 
 // Our basic server
 var server;
+var serverPath = "http://localhost:7500";
 
 before(function(done) {
   server = http.createServer(function(request, response) {
-    response.statusCode = 200;
+    
+    var statusCode;
+    var responseText;
+
+    if (request.url !== '/') {
+      statusCode = 404;
+      responseText = 'Sorry, your request is bogus.';
+    } else {
+      statusCode = 200;
+      responseText = 'Hello, Node!';
+    }
+
+    response.writeHead(statusCode, {
+      'content-type': 'text/plain' 
+    });
+    response.write(responseText);
+
     response.end();
   });
   server.listen(7500);
   done();
 });
 
-before(function(done) {
+after(function(done) {
+  server.close();
   done();
 });
 
@@ -21,20 +40,52 @@ describe.only('User makes HTTP request', function() {
 
   context('Given a request URL of "/"', function() {
     it('should send a response with a 200 HTTP status code', function(done) {
-      var serverPath = "http://localhost:7500";
-      http.get(serverPath, function(response) {
-        assert.equal(response.statusCode, 200);
+      request(function(err, res){
+        assert.equal(res.statusCode, 200);
+        done();
+      })
+    });
+    
+    it('should respond with the text "Hello, Node!"', function(done) {
+      request(function(err, res){
+        assert.equal(res.text, 'Hello, Node!');
         done();
       });
     });
-    it('should respond with the text "Hello, Node!"');
-    it('should send the response as plain text');
+
+    it('should send the response as plain text', function(done) {
+      request(function(err, res){
+        assert.equal(res.headers['content-type'], 'text/plain');
+        done();
+      });
+    });
   });
 
   context('Given a request URL other than "/"', function() {
-    it('should send a response with a 404 HTTP status code');
-    it('should respond with the text "Sorry, your request is bogus."');
-    it('should send the response as plain text');
+    it('should send a response with a 404 HTTP status code', function(done) {
+      superagent.get(serverPath + '/hello').end(function(err, res) {
+        assert.equal(res.statusCode, 404);
+        done();
+      })
+    });
+
+    it('should respond with the text "Sorry, your request is bogus."', function(done) {
+      superagent.get(serverPath + '/hello').end(function(err, res) {
+        assert.equal(res.text, 'Sorry, your request is bogus.');
+        done();
+      })
+    });
+
+    it('should send the response as plain text', function(done) {
+      superagent.get(serverPath + '/hello').end(function(err, res) {
+        assert.equal(res.headers['content-type'], 'text/plain');
+        done();
+      })
+    });
   });
 
 });
+
+function request(cb) {
+  superagent.get(serverPath).end(cb);
+};
